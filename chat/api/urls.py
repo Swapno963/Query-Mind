8a38650394cf62e09f92f3654e978075678cbdf2
swapno963 <swapno963@gmail.com>
2026-08-23 23:29:24 +0600

    test

--- chat/api/urls.py
+++ chat/api/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path(
         "chat/result/",
         ChatResultAPIView.as_view(),
-        name="api-chat-test",
+        name="api-chat",
     ),
     path("health", health_check),
 ]
